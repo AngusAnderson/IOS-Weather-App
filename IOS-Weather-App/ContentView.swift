d406d9475cd2d8bdcc2a44a9b7bc7f8d4c5fdbdd
Angus Anderson <angus.anderson.2006@gmail.com>
2026-09-17 21:04:47 +0100

    Initial commit: WeatherApp

--- IOS-Weather-App/ContentView.swift
+++ IOS-Weather-App/ContentView.swift
@@ -1,17 +1,8 @@
 import SwiftUI
-import Playgrounds
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Hello, World!")
             .padding()
     }
-}
-
-#Preview {
-    ContentView()
-}
-
-#Playground {
-    _ = 1 + 2
 }
